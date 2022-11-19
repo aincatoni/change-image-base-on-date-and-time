b6e9a55cc8bee77212ef7e928bd45b71e1a01de5
aincatoni <ain.catoni@gmai.com>
2022-11-18 22:38:42 -0300

    mejora la lógica y el orden del script

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-const header = document.getElementById("header");
-function addDay() {
- const date = new Date();
- const d = date.getDate();
- const h = date.getHours();
- const m = date.getMinutes();
- const s = date.getSeconds();
+// Este script es para cambiar el header a el día y la hora indicada
 
- if (d <= 18 && h === 12 && m > 10) {
-  header.src = "./img/img_02.png";
-  console.log("works");
+// Ejecuta el código una vez cargado el sitio
+document.onreadystatechange = function () {
+ const header = document.getElementById("header");
+
+ if (document.readyState == "interactive") {
+  function changeHeader() {
+   // Guarda el mes, día, hora, minutos y segundos en variables
+   const date = new Date();
+   const mn = date.getMonth();
+   const d = date.getDate();
+   const h = date.getHours();
+   const m = date.getMinutes();
+   const s = date.getSeconds();
+
+   // Comprueba que el mes, día, hora, y minutos sean:
+
+   if (mn >= 10 && d >= 18 && h >= 22 && m >= 38) {
+    header.src = "./img/img_02.png";
+   }
+  }
+  changeHeader();
+  setInterval(changeHeader, 0.01);
  }
-}
-
-window.setInterval(addDay, 10);
+};
